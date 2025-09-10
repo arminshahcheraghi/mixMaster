a156807b07cfd40fb537a4339296eaee83892db7
arminshahcheraghi <armin75sh@gmail.com>
2025-09-10 10:02:31 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+  HomeLayout,
+  About,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+} from './pages';
+
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleCocktailLoader } from "./pages/Cocktail";
+
+import { action as newsletterAction } from "./pages/Newsletter";
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime: 1000 * 60 * 5
+    }
+  }
+});
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing/>,
+        loader: landingLoader(queryClient)
+      },
+      {
+        path:'/cocktail/:id',
+        element: <Cocktail/>,
+        loader: singleCocktailLoader(queryClient)
+      },
+      {
+        path:'/newsletter',
+        action: newsletterAction,
+        element: <Newsletter/>
+      },
+      {
+        path:'/about',
+        element: <About/>
+      },
+
+    ]
+  },
+]);
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+};
+export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+    return <div>
+        <h1>About page</h1>
+        <Link to='/'>Back Home</Link>
+    </div>
+};
+export default About;
